5c3762b5533cfdbcb718360e0c2e109bdd66956e
Amresh Maurya <amresh.terminal@gmail.com>
2023-07-23 23:26:17 +0530

    improved margin issue

--- app/pages/Navbar.js
+++ app/pages/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
    
 
-    <div className="z-50 flex flex-col justify-evenly py-2 px-2 gap-6 sm:gap-4 sm:flex-row md:flex-col  sm:w-[90vw] shadow-xl bg-secondary text-white  rounded-2xl" >
+    <div className="z-50 sm:mb-2 flex flex-col justify-evenly py-2 px-2 gap-6 sm:gap-4 sm:flex-row md:flex-col  sm:w-[90vw] shadow-xl bg-secondary text-white  rounded-2xl" >
 
 <Link href="/"><RiHomeHeartFill  className="text-2xl sm:text-2xl" /></Link>
 <Link href="work"><SiPolymerproject className="text-2xl sm:text-2xl" /></Link>
