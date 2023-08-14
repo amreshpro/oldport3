53570516b57f4c84d5341336e51f6ddc866c6e7e
Amresh Maurya <amresh.terminal@gmail.com>
2023-08-14 19:04:41 +0530

    almost done and fix issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,30 @@ export default {
       // => @media (max-width: 639px) { ... }
     },
 
+    colors:{
+      'primary':"#0C4A6E",
+      'primaryLight':'#D946EF',
+      'secondary':"#0891B2",
+      'ternary':"#BEF264",
+      'white':"#ffffff",
+      'black':"#000000",
+      'gray':"#F8FBFD",
+      'green':"#A3E635",
+      'red':"#FCA5A5",
+      'blue':"#2563EB",
+      "purple":"#A855F7",
+      "gold":"#FBBF24",
+      "tree":"#65A30D",
+      "blueGray":"#94A3B8",
+      "purple400":"#C084FC",
+      "pink600":"#DB2777",
+      "emraldLight":"#6EE7B7",
+      "bar":"#06B6D4",
+      "github":"#1F2937",
+      "play":"#F97316"
+          },
+
+
     extend: {},
   },
 }
