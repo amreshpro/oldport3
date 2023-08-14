3ec91e3508ca5066b910626a084b17bc95e1fe01
Amresh Maurya <amresh.terminal@gmail.com>
2023-08-14 16:34:47 +0530

    start: basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+
+    screens: {
+      '2xl': { 'max': '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
+      'xsm': { 'max': '339px' },
+      // => @media (max-width: 639px) { ... }
+    },
+
+    extend: {},
+  },
+}
+
