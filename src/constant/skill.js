6f941e17ed05bdd32c1109f876d870510a0fc8d5
Amresh Maurya <amresh.terminal@gmail.com>
2023-08-14 19:01:34 +0530

    add nad fix constants

---
+++ src/constant/skill.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+const skills = [
+    { id:"sk1",
+      name: "JavaScript",
+  
+    },
+     
+    {id:"sk2",
+      name: "React.js",
+  
+    },
+    {id:"sk3",
+    name: "Typescript",
+
+  },
+    {id:"sk4",
+      name: "Node.js",
+  
+    },
+
+  
+    {id:"sk5",
+      name: "Express.js",
+  
+    },
+    
+    {id:"sk6",
+      name: "Tailwind CSS",
+   
+    },
+    {id:"sk7",
+      name: "MongoDB",
+  
+    },
+    {  id:"sk8",
+    name: "Threejs",
+  
+  },
+    {  id:"sk9",
+      name: "Git",
+   
+    },
+  
+    {   id:"sk10",
+    name: "Python",
+
+  },
+    {   id:"sk11",
+      name: "CSS",
+  
+    },
+  
+    {   id:"sk12",
+      name: "HTML",
+   
+    },
+  
+  ];
+  
+  export default skills;
---
+++ src/constant/socialLinks.js
@@ -0,0 +1,13 @@
+
+
+
+const socialLinks ={
+    instagram:"https://instagram.com/ursamresh",
+    twitter:"https://twitter.com/ursamresh",
+    github:"https://github.com/amreshpro",
+    linkedin:"https://linkedin.com/in/ursamresh",
+    portfolio:"https://amreshpro.com",
+    resume:"https://drive.google.com/file/d/1Br8ZWK2uhQ9teX75GbFAh8kxg__JMMQS/view?usp=drive_link"
+}
+    
+    export default socialLinks
