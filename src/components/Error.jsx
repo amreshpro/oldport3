8ee122787ddd718717706abbd3a724303eee06b2
Amresh Maurya <amresh.terminal@gmail.com>
2023-08-14 22:25:48 +0530

    improved error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="flex gap-4 flex-col flex-wrap w-full mt-4 text-center justify-center items-center text-4xl">
+
+<p className="text-red">Page Not Found : 404</p>
+
+<Link to="/" className="bg-secondary w-max text-center text-white text-2xl p-2 rounded-lg ">Back To Home</Link>
+    </div>
   )
 }
 export default Error
