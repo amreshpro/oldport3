c3102360ca50760d156c21c5aa78a7d6c3d458d7
Amresh Maurya <amresh.terminal@gmail.com>
2023-09-02 10:57:16 +0530

    improved comma mistake

--- src/constant/about.js
+++ src/constant/about.js
@@ -1,5 +1,5 @@
 
 
-export const intro = "Determined individual looking for an engineering job with detailed knowledge in Javascript, Reactjs, Nextjs, Expressjs, Nodejs, TailwindCSS, Java, Python,, MongoDB. An added advantage of knowing Linux,Docker. Ability to write, test, maintain, and debug codes."
+export const intro = "Determined individual looking for an engineering job with detailed knowledge in Javascript, Reactjs, Nextjs, Expressjs, Nodejs, TailwindCSS, Java, Python, MongoDB. An added advantage of knowing Linux,Docker. Ability to write, test, maintain, and debug codes."
 
 export const oneliner = "I code beautifully simple things.I enjoy creating things that live on internet and I love what I do."
