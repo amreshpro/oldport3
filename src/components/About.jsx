3bc95681126f78bf553d5213d5c39118f225bceb
Amresh Maurya <amresh.terminal@gmail.com>
2023-09-19 00:36:46 +0530

    fix image width issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { intro1,intro2,intro3,key1name,key1des,key2name,key2des,key3name,key3des
 
 const About = () => {
   return (
-    <div className="flex flex-col gap-4 h-screen w-full  mx-8 sm:mx-2 sm:mr-0 sm:ml-0 px-16 sm:px-4 py-4">
+    <div className="flex flex-col gap-4 h-full w-full  mx-8 sm:mx-2 sm:mr-0 sm:ml-0 px-16 sm:px-4 py-4">
 
 
 <div id="about" className="flex gap-1 "><p className="text-ternary font-mono text-xl ">01.</p> <p className="hover:text-ternary text-lightest-slate font-nunito text-4xl font-bold">About</p> <div className="h-[1px] w-36 bg-slate hover:bg-ternary ml-0.5 mt-4"></div> </div>
