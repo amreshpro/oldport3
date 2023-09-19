48fecb14d757f630f90d72ab29adff043dab2b76
Amresh Maurya <amresh.terminal@gmail.com>
2023-09-19 11:37:04 +0530

    remove margin from top in work

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -12,7 +12,7 @@ const Works = () => {
 
 
   return (
-    <div className=" bg-primary flex flex-wrap flex-col gap-4 justify-center  h-full sm:mt-56  ml-8 sm:mr-0 sm:ml-0 px-16 sm:px-4 py-4">
+    <div className=" bg-primary flex flex-wrap flex-col gap-4 justify-center  h-full mt-4  ml-8 sm:mr-0 sm:ml-0 px-16 sm:px-4 py-4">
 
       <div id="work" className="flex gap-1 justify-items-center "><p className="text-ternary font-mono text-xl ">02.</p> <p className="hover:text-ternary text-lightest-slate font-nunito text-4xl font-bold">Work</p> <div className="h-[1px] w-36 bg-slate hover:bg-ternary ml-0.5 mt-4"></div> </div>
 
